30ae8d8478ff3de2b57edccf4f84d3f9bddd5ef4
Alex <alun7358@colorado.edu>
2025-04-22 13:17:49 -0600

    Update overlap.py

--- overlap.py
+++ overlap.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python
 
-
-#start with activating conda envs containing pychimera stuff
+#start with activating conda envs containing pychimera info from chimera_installation.sh
 #needs to start with just pychimera, otherwise chimera will not be imported
-#would suggest typing "pychimera" into the command line, then running the script with python that way
-#that way you can perform this from any folder :3
 pychimera
-import chimera 
-from chimera import openModels, Molecule, UserError, numpyArrayFromAtoms
 
+#import chimera dependencies
+import chimera
+from chimera import openModels, Molecule, UserError, numpyArrayFromAtoms, Xform
+import pandas as pd
+import biopandas as bp
+from biopandas.pdb import PandasPdb as ppdb
+import os
+import pawk
+
+#read the pdb file that you want to adjust 
+#assumes it's within the current directory
+ppdb.read_pdb('$/5o3l.pdb')
+
+#define the atoms 
+atom=ppdb.df['ATOM']
+x=ppdb.df[$7]
+y=ppdb.df[$8]
+z=ppdb.df[$9]
+
+#for loop, checking the coordinates in the pdb file to determine significant overlap
+for i in atom:
