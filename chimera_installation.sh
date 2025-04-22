15fa57ad3c9465eda3236d2697da4988a392f01e
Alex <alun7358@colorado.edu>
2025-04-21 21:43:54 -0600

    Update chimera_installation.sh

--- chimera_installation.sh
+++ chimera_installation.sh
@@ -12,10 +12,13 @@ in_dir="/projects/<identikey>/software/anaconda/envs/chimera"
 #run the installation script and move to main directory
 bash $in_dir/scripts/install_chimera.sh
 mv chimera-1.13.1-linux_x86_64.bin $in_dir
+
 #unzip that bin file into a different folder
 unzip $in_dir/chimera-1.13.1-linux_x86_64.bin -d $in_dir/chimera_install
+
 #to further unzip and get files
 $in_dir/chimera_install/chimera.bin
+
 #write the path to the final chimera directory
 export CHIMERADIR="$in_dir" 
 #if it gives the error: No space left on device: '/home/alun7358/.chimera', execute the following to move it and solve the problem:
