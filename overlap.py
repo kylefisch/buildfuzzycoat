9e2b343f0a6751a4383f1c2994d9c6da3aea565d
Alex <alun7358@colorado.edu>
2025-04-22 12:04:58 -0600

    Update overlap.py

--- overlap.py
+++ overlap.py
@@ -2,6 +2,7 @@
 
 #needs to start with just pychimera, otherwise chimera will not be imported
 #would suggest typing "pychimera" into the command line, then running the script with python that way
+#that way you can perform this from any folder :3
 pychimera
 import chimra 
 
