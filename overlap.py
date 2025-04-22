f98fc7c25f3b3b4d622c91f3a222cf0f0a39bfba
Alex <alun7358@colorado.edu>
2025-04-22 12:05:31 -0600

    Update overlap.py

--- overlap.py
+++ overlap.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+
+#start with activating conda envs containing pychimera stuff
 #needs to start with just pychimera, otherwise chimera will not be imported
 #would suggest typing "pychimera" into the command line, then running the script with python that way
 #that way you can perform this from any folder :3
